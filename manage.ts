021893137c851435ff79f888d68f5c1a9d84e8ca
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2021-09-25 04:31:55 +0500

    cleanup

--- manage.ts
+++ manage.ts
@@ -11,40 +11,14 @@ if (args.length == 0) {
 
 // TOOD: write argument parser and handle
 
-let chosen: string[] | null = null;
-const commands: {
-  commands: { [key: string]: string[] };
-} = JSON.parse(await Deno.readTextFile("./commands.json"));
-
-if (Deno.build.os === "windows") {
-  for (const entry of Object.keys(commands.commands)) {
-    commands.commands[entry].unshift("cmd", "/C");
-  }
-}
-
-chosen = commands.commands[args[0].toString().replace(/\-\-/gi, "")];
-
-if (chosen) {
-  const p = Deno.run({
-    cmd: chosen,
-    stdout: "piped",
-    stderr: "piped",
-  });
-
-  const { code } = await p.status();
-
-  const rawOutput = await p.output();
-  const rawError = await p.stderrOutput();
-
-  if (code === 0) {
-    await Deno.stdout.write(rawOutput);
-  } else {
-    const errorString = new TextDecoder().decode(rawError);
-    console.log(errorString);
-  }
-
-  Deno.exit(code);
-} else {
-  messages.help();
-  Deno.exit(1);
+switch (args[0]) {
+  case 'install':
+    break;
+  case 'uninstall':
+    break;
+  case 'search':
+    break;
+  default:
+    messages.help();
+    Deno.exit(1);
 }
